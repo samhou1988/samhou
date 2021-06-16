12386dce26b89076bbbbff841e9b9bed7e922327
samhou <samhou@tencent.com>
2021-06-16 18:13:17 +0800

    feat: cli-welcome

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const welcome = require('cli-welcome');
+const pkg = require('./package.json');
+
+welcome({
+  title: pkg.name,
+  tagLine: `Get to know Sam`,
+  description: pkg.description,
+  version: pkg.version,
+  color: '#000000',
+  bgColor: '#f00',
+  bold: true,
+  clear: true
+});
 
 console.log(`
 sam hou
